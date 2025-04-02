f68e2f921ab9c0fe23adc56cc762b5ffa137440a
thaivanhoa37 <hoatv.21it@vku.udn.vn>
2025-04-02 11:59:08 +0100

    thêm 2 cử chỉ

--- collect_imgs.py
+++ collect_imgs.py
@@ -11,7 +11,7 @@ DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-number_of_classes = 3
+number_of_classes = 5
 dataset_size = 100
 
 for j in range(number_of_classes):
